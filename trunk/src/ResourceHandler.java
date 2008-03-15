cef5de78c6e8d4a8b94de6462cc8e32f16423375
npomfret@gmail.com <npomfret@gmail.com@3e9514dd-c933-0410-adaa-93700b1f64e1>
2008-03-15 23:41:57 +0000

    added 'finished' message

--- trunk/src/ResourceHandler.java
+++ trunk/src/ResourceHandler.java
@@ -1,6 +1,5 @@
-import java.io.IOException;
 import java.io.InputStream;
 
 interface ResourceHandler {
-    void handleStream(int length, InputStream is, String url) throws IOException, InterruptedException;
+    void handleStream(int length, InputStream is, String url) throws InterruptedException;
 }
